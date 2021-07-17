35f672f89b6cf6fa8353e039b66255e368d1b5af
Jose Salvador Guevara Lunar <jsalvag@gmail.com>
2021-07-17 17:21:42 -0300

    Creating a URL endpoint by Jose Guevara

--- encuestas/views.py
+++ encuestas/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def gretting(request):
     return HttpResponse("Hello world from Django for Codo a Codo 4.0")
+
+def jose(request):
+    return HttpResponse("Esta es la respuesta creada por Jose Guevara")
