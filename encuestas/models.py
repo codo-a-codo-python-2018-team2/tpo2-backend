9f9672ebe27be1a325764d4d1d995d1d29e3df26
Jose Salvador Guevara Lunar <jsalvag@gmail.com>
2021-07-17 20:38:55 -0300

    Agregando directorio de datos al .gitignore

--- encuestas/models.py
+++ encuestas/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
 class User(models.Model):
-    username = models.CharField(max_length=32) # Acabo de craer una tabla que tiene una property que es un var char de 32 caracteres
+    username = models.CharField(max_length=32)  # Acabo de crear una tabla que tiene una property que es un var char
+    # de 32 caracteres
     dni = models.PositiveIntegerField()
-    
